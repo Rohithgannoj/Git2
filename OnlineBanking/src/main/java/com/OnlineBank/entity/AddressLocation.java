d6a7cbb2fc55dd247493d06e83f0dedfe4e4d3c1
TL <TL@gmail.com>
2022-06-13 23:04:20 +0530

    new Push

---
+++ OnlineBanking/src/main/java/com/OnlineBank/entity/AddressLocation.java
@@ -0,0 +1,37 @@
+package com.OnlineBank.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GeneratorType;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+
+public class AddressLocation {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	@Column(nullable = false,name ="houseNumber",length = 10)
+	private String hno;
+	@Column(nullable = false,length = 20)
+	private String street;
+	@Column(nullable = false,length = 20)
+	private String city;
+	@Column(nullable = false,length = 15)
+	private String state;
+	@Column(nullable = false,length = 6)
+	private Long pincode;
+}
---
+++ OnlineBanking/src/main/java/com/OnlineBank/entity/SavingsAccount.java
@@ -0,0 +1,44 @@
+package com.OnlineBank.entity;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "SavingsAccounts")
+public class SavingsAccount {
+
+	@Id
+	@SequenceGenerator(initialValue = 120000000, name = "SavingsAccountSeries")
+	@Column(nullable = false, length = 10, unique = true)
+	private Long accountNumber;
+	
+	@Column(nullable = false, length = 10)
+	private String accountHolderName;
+	
+	@Column(nullable = false)
+	private Date dateOfBirth;
+	
+	@Column(nullable = false)
+	private String fatherName;
+	
+	@OneToOne
+	@MapsId
+	private AddressLocation addressLocation;
+	
+	@Column(nullable = false)
+	private Long phoneNumber;
+
+}
